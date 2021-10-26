980f2d1aa291b3c8e4691e3beea29f98321ab846
grishanych <alsolo2011@gmail.com>
2021-10-26 11:05:31 +0300

    Changed route components to functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
-  return (
-    <Router>
-    <div className="App">
-     </div>
-     <Switch>
-        <Route path='/' exact component={Main}/>
-        <Route path='/clubs' component={Clubs}/>
-        <Route path='/challenge' component={Challenge}/>
-        <Route path='/about' component={About}/>
-        <Route path='/service' component={Service}/> 
-        
-        <Route component={NotFound}/>
-        </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+            </div>
+            <Switch>
+                <Route path='/' exact component={() => (<div>Main Route</div>)} />
+                <Route path='/clubs' component={() => (<div>Clubs Route</div>)} />
+                <Route path='/challenge' component={() => (<div>Clubs Challenge</div>)} />
+                <Route path='/about' component={() => (<div>About Route</div>)} />
+                <Route path='/service' component={() => (<div>Service Route</div>)} />
+                <Route component={() => (<div>Not found Route</div>)} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
