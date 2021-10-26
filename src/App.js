090915e5e0b7b7011bf0e793c6ca4330bb9522e6
grishanych <alsolo2011@gmail.com>
2021-10-26 12:32:01 +0300

    Create base routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
-  return (
-    <div className="App">
-      <Footer/>
- 
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Footer />
+
+            </div>
+            <Switch>
+                <Route path='/' exact component={() => (<div>Main Route</div>)} />
+                <Route path='/clubs' component={() => (<div>Clubs Route</div>)} />
+                <Route path='/challenge' component={() => (<div>Clubs Challenge</div>)} />
+                <Route path='/about' component={() => (<div>About Route</div>)} />
+                <Route path='/service' component={() => (<div>Service Route</div>)} />
+                <Route component={() => (<div>Not found Route</div>)} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
