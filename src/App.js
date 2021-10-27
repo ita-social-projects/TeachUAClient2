55f733d217cb4fe390fe7d7c462bb42e3e79bb95
Oleksandr Sovynskyi <oleksandrsov@gmail.com>
2021-10-27 17:04:56 +0300

    init eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
-import Header from './Components/header/header'
-
-
+import Header from './Components/header/header';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Footer />
-            </div>
-            <Switch>
-                <Route path='/' exact component={() => (<div>Main Route</div>)} />
-                <Route path='/clubs' component={() => (<div>Clubs Route</div>)} />
-                <Route path='/challenge' component={() => (<div>Clubs Challenge</div>)} />
-                <Route path='/about' component={() => (<div>About Route</div>)} />
-                <Route path='/service' component={() => (<div>Service Route</div>)} />
-                <Route component={() => (<div>Not found Route</div>)} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Footer />
+      </div>
+      <Switch>
+        <Route path="/" exact component={() => (<div>Main Route</div>)} />
+        <Route path="/clubs" component={() => (<div>Clubs Route</div>)} />
+        <Route path="/challenge" component={() => (<div>Clubs Challenge</div>)} />
+        <Route path="/about" component={() => (<div>About Route</div>)} />
+        <Route path="/service" component={() => (<div>Service Route</div>)} />
+        <Route component={() => (<div>Not found Route</div>)} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
