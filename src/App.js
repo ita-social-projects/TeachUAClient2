b81b165ad8d797cae401ffc779d7adf39b261b0c
Sel1nd <35139107+Sel1nd@users.noreply.github.com>
2021-10-29 14:30:21 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/header/header';
+import MainPage from './Components/mainPage/mainPage';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
-      <Switch>
-        <Route path="/" exact component={() => (<div>Main Route</div>)} />
-        <Route path="/clubs" component={() => (<div>Clubs Route</div>)} />
-        <Route path="/challenge" component={() => (<div>Clubs Challenge</div>)} />
-        <Route path="/about" component={() => (<div>About Route</div>)} />
-        <Route path="/service" component={() => (<div>Service Route</div>)} />
-        <Route component={() => (<div>Not found Route</div>)} />
-      </Switch>
-    </Router>
+    <div className="App">
+     <Header/> 
+    </div>
+    <Switch>
+        <Route path='/' exact  component={() => (<MainPage />)}/>
+        <Route path='/clubs' component={() => (<div>Clubs Route</div>)}/>
+        <Route path='/challenge' component={() => (<div>Clubs Challenge</div>)}/>
+        <Route path='/about' component={() => (<div>About Route</div>)}/>
+        <Route path='/service' component={() => (<div>Service Route</div>)}/>
+        <Route component={() => (<div>Not found Route</div>)}/>
+    </Switch>
+    <Footer/>
+</Router>
   );
 }
 
