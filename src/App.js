0183a31dc0f78246b2bac92ae609346911920bd2
grishanych <alsolo2011@gmail.com>
2021-11-05 01:38:37 +0200

    Add click on button in header, add show/hide menu checkbox

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Footer from './Components/footer/Footer';
-
+import Footer from './Components/Footer/Footer';
 import Header from './Components/header/header';
 import MainPage from './Components/mainPage/mainPage';
 import { LeftSearch } from './Components/left_side_search/left_side_search';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.togleSideSearch = this.togleSideSearch.bind(this);
+        this.state = {
+            show: false,
+        }
+    }
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        
-        <LeftSearch />
-    
+    togleSideSearch() {
+        this.setState({ show: !this.state.show })
+    }
 
-      </div>
-      <Switch>
-        <Route path="/" exact component={() => (<MainPage/ >)} />
-        <Route path="/clubs" component={() => (<div>Clubs Route</div>)} />
-        <Route path="/challenge" component={() => (<div>Clubs Challenge</div>)} />
-        <Route path="/about" component={() => (<div>About Route</div>)} />
-        <Route path="/service" component={() => (<div>Service Route</div>)} />
-        <Route component={() => (<div>Not found Route</div>)} />
-      </Switch>
-      <Footer />
-
-    </Router>
-   
-  );
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Header togleSideSearch={this.togleSideSearch} />
+                    {this.state.show ? <LeftSearch /> : null}
+                </div>
+                <Switch>
+                    <Route path="/" exact component={() => (<MainPage />)} />
+                    <Route path="/clubs" component={() => (<div>Clubs Route</div>)} />
+                    <Route path="/challenge" component={() => (<div>Clubs Challenge</div>)} />
+                    <Route path="/about" component={() => (<div>About Route</div>)} />
+                    <Route path="/service" component={() => (<div>Service Route</div>)} />
+                    <Route component={() => (<div>Not found Route</div>)} />
+                </Switch>
+                <Footer />
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -12,98 +12,105 @@ import Plate from "../header_img/plate.svg";
 import menuIcon from "../header_img/menu.svg";
 import { Menu, Dropdown } from "antd";
 import "./header.scss";
-export class header extends Component {
-  render() {
-    return (
-      <div className="Header">
-        <div className="wrapper">
-          <img className="logo" src={Logo} />
-          <div className="Menu">
-            <label className="burger-icon" htmlFor="burger-checkbox">
-              <img src={menuIcon} />
-            </label>
-            <input type="checkbox" id="burger-checkbox" />
-            <nav>
-              <a href="#blank">
-                <img src={ProjectIcon} />
-                Гуртки
-              </a>
-              <Dropdown overlay={menu}>
-                <a className="challenge" href="#blank">
-                  <img src={Crown} />
-                  Челендж
-                </a>
-              </Dropdown>
-              <a className="news" href="#blank">
-                <img src={NewsIcon} />
-                Про нас
-              </a>
-              <a className="project" href="#blank">
-                <img src={Flag} />
-                Послуги українською
-              </a>
-            </nav>
-          </div>
-          <div className="log">
-            <div className="locationIcon">
-              <img src={Loaction} />
-              <div className="city">
-                <p>Місто</p>
-              </div>
-            </div>
+import PropTypes from 'prop-types';
 
-            <Dropdown overlay={log}>
-              <div className="avatar">
-                <img src={Avatar} />
-              </div>
-            </Dropdown>
-          </div>
-        </div>
-        <div className="LowerWrappper">
-          <p className="caption">Ініціатива &quot;Навчай українською&quot;</p>
-          <div className="search">
-            <div className="input-field">
-              <div className="search-bar">
-                <input type="text" placeholder="Який гурток шукаєте?"></input>
-                <img src={Lens} />
-              </div>
-              <img className="plaate" src={Plate} />
+
+export class header extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div className="Header">
+                <div className="wrapper">
+                    <img className="logo" src={Logo} />
+                    <div className="Menu">
+                        <label className="burger-icon" htmlFor="burger-checkbox">
+                            <img src={menuIcon} />
+                        </label>
+                        <input type="checkbox" id="burger-checkbox" />
+                        <nav>
+                            <a href="#blank">
+                                <img src={ProjectIcon} />
+                                Гуртки
+                            </a>
+                            <Dropdown overlay={menu}>
+                                <a className="challenge" href="#blank">
+                                    <img src={Crown} />
+                                    Челендж
+                                </a>
+                            </Dropdown>
+                            <a className="news" href="#blank">
+                                <img src={NewsIcon} />
+                                Про нас
+                            </a>
+                            <a className="project" href="#blank">
+                                <img src={Flag} />
+                                Послуги українською
+                            </a>
+                        </nav>
+                    </div>
+                    <div className="log">
+                        <div className="locationIcon">
+                            <img src={Loaction} />
+                            <div className="city">
+                                <p>Місто</p>
+                            </div>
+                        </div>
+
+                        <Dropdown overlay={log}>
+                            <div className="avatar">
+                                <img src={Avatar} />
+                            </div>
+                        </Dropdown>
+                    </div>
+                </div>
+                <div className="LowerWrappper">
+                    <p className="caption">Ініціатива &quot;Навчай українською&quot;</p>
+                    <div className="search">
+                        <div className="input-field">
+                            <div className="search-bar">
+                                <input type="text" placeholder="Який гурток шукаєте?"></input>
+                                <img src={Lens} />
+                            </div>
+                            <img className="plaate" src={Plate} />
+                        </div>
+                        <div className="litle-toggle">
+                            <a href="#blank">
+                                <img className="toggle" src={Toggle} onClick={this.props.togleSideSearch} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="litle-toggle">
-              <a href="#blank">
-                <img className="toggle" src={Toggle} />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 const menu = (
-  <Menu>
-    <Menu.Item>
-      <a target="#blank">Навчай українською Челендж</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="#blank">Мовомаратон</a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="#blank">Навчай українською</a>
-    </Menu.Item>
-  </Menu>
+    <Menu>
+        <Menu.Item>
+            <a target="#blank">Навчай українською Челендж</a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="#blank">Мовомаратон</a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="#blank">Навчай українською</a>
+        </Menu.Item>
+    </Menu>
 );
 
 const log = (
-  <Menu>
-    <Menu.Item>
-      <Registration/>
-    </Menu.Item>
-    <Menu.Item>
-      <Login/>
-    </Menu.Item>
-  </Menu>
-  
+    <Menu>
+        <Menu.Item>
+            <Registration />
+        </Menu.Item>
+        <Menu.Item>
+            <Login />
+        </Menu.Item>
+    </Menu>
+
 );
+header.propTypes = {
+    togleSideSearch: PropTypes.func
+};
 
 export default header;
