49850cfd88a8f29f8bf199fa44876b1a3950565e
tytomyr <art.tytomyr@gmail.com>
2021-11-04 14:16:54 +0200

    resolving conflicts

--- src/Components/left_side_search/left_side_search.js
+++ src/Components/left_side_search/left_side_search.js
@@ -30,25 +30,6 @@ class LeftSearch extends Component {
                             <Radio value={2}><span className='advancedSearchSpan'>Центр</span></Radio>
                         </Radio.Group>
                         <p className='advancedSearchTitle'>Місто</p>
-<<<<<<< HEAD
-                        <Select placeholder="Виберіть місто" className='selectStyle'>
-                            <Option value="kyiv">Київ</Option>
-                            <Option value="kharkiv">Харків</Option>
-                            <Option value="dnipro">Дніпро</Option>
-                            <Option value="odesa">Одеса</Option>
-                            <Option value="zaporizzhya">Запоріжжя</Option>
-                            <Option value="kropyvnytskyi">Кропивницький</Option>
-                            <Option value="herson">Херсон</Option>
-                            <Option value="mykolaiv">Миколаїв</Option>
-                            <Option value="sumy">Суми</Option>
-                            <Option value="mariupol">Маріуполь</Option>
-                            <Option value="chernihiv">Чернігів</Option>
-                            <Option value="poltava">Полтава</Option>
-                            <Option value="kremenchuk">Кременчук</Option>
-                            <Option value="cherkasy">Черкаси</Option>
-                            <Option value="without_location">Без локації</Option>
-                        </Select>
-=======
                         <label>
                             <Select className='selectStyle' value={this.state.value} onChange={this.handleChange} placeholder='Виберіть місто...' allowClear>
                                 <Option value="Київіі">Київ</Option>
@@ -69,7 +50,6 @@ class LeftSearch extends Component {
                             </Select>
                         </label>
 
->>>>>>> develop
                         <p className='advancedSearchTitle'>Район міста</p>
                         <Select placeholder="Виберіть місто" className='selectStyle'>
                             <Option value="kyiv"><img className='imageNoData' src={no_data} /></Option>
@@ -110,22 +90,9 @@ class LeftSearch extends Component {
                         }} /><span className='advancedSearchSpanYears'>років</span>
                     </form>
                 </div>
-<<<<<<< HEAD
             </div>
-=======
-
-
-            </div>
-
-
->>>>>>> develop
         )
     }
 }
 
-<<<<<<< HEAD
 export { LeftSearch };
-=======
-export { LeftSearch };
-
->>>>>>> develop
