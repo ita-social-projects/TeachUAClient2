91ecb721605901e394fa826dd444263b08f903cc
Sel1nd <35139107+Sel1nd@users.noreply.github.com>
2021-11-04 14:57:07 +0200

    merge develop

--- src/Components/mainPage/mainPage.js
+++ src/Components/mainPage/mainPage.js
@@ -1,18 +1,10 @@
 import React from "react";
 import 'antd/dist/antd.css';
 import "./mainPage.scss";
-<<<<<<< HEAD:src/Components/mainPage/mainPage.js
-
 import Banner from './Banner/Banner';
 import GroupDirection from './GroupDirection/GroupDirection';
 import Information from './Information/Information';
 import Slider from './Slider/Slider';
-=======
-import Banner from '../mainPage/Banner/Banner';
-import GroupDirection from '../mainPage/GroupDirection/GroupDirection';
-import Information from '../mainPage/Information/Information';
-import Slider from '../mainPage/Slider/Slider';
->>>>>>> develop:src/Components/mainPage/mainPage.jsx
 
 
 import {Layout} from 'antd';
