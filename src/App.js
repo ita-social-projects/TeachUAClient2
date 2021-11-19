636580bbcfcb0e3f8e63ad331dd95c1e0308ccbe
Sel1nd <35139107+Sel1nd@users.noreply.github.com>
2021-11-19 11:00:39 +0200

    profile was added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/header/header';
 import MainPage from './Components/mainPage/mainPage';
 import Clubs from './Components/clubs/clubs';
+import Profile from './Components/profile/profile';
 import { LeftSearch } from './Components/left_side_search/left_side_search';
 
 class App extends Component {
@@ -27,12 +28,14 @@ class App extends Component {
                     <Header toggleSideSearch={this.toggleSideSearch} />
                     {this.state.show ? <LeftSearch /> : null}
                 </div>
+                
                 <Switch>
                     <Route path="/" exact component={() => (<MainPage />)} />
                     <Route path="/clubs" component={() => (<Clubs/>)} />
                     <Route path="/challenge" component={() => (<div>Clubs Challenge</div>)} />
                     <Route path="/about" component={() => (<div>About Route</div>)} />
                     <Route path="/service" component={() => (<div>Service Route</div>)} />
+                    <Route path="/profile" component={() => (<Profile />)} />
                     <Route component={() => (<div>Not found Route</div>)} />
                 </Switch>
                 <Footer />
---
+++ src/Components/profile/profile.js
@@ -0,0 +1,65 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import "./profile.scss";
+import logo from "./avatar.svg"
+import mail from "./mail.svg"
+import profile from "./Vector.svg"
+
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div className="profile">
+          <div className="profile-office-block">
+          <div className="profile-office-block__wrapper">
+            <h3>Особистий кабінет</h3>
+            <div className="profile-office-block__content">
+              <div className="profile-office-block__profile">
+                <img src={profile}></img>
+                <p>Профіль</p>
+              </div>
+              <div className="profile-office-block__message">
+              <img src={mail}></img>
+                <p>Повідомлення</p>
+              </div>
+              </div>
+          </div>
+          </div>
+          <div className="profile-information-block">
+              <div className="profile-information-block__wrapper">
+                <h2>Мій профіль</h2>
+                <div className="profile-information-block__card">
+                    <div className="profile-information-block__content-left">
+                        <div className="profile-information-block__name-wrapper">
+                            <div className="profile-information-block__name">
+                              <div className="name-wrapper">
+                                <img src={logo} />
+                                <div className="name">
+                                  <h2>Арсен Шаріфов</h2>
+                                  <h3>ROLE_MANAGER</h3>
+                                </div>
+                              </div>
+                            </div>
+                        </div>
+                        <div className="number-wrapper">
+                              <h4>Телефон</h4>
+                              <p>+38 (096) 227 45 68</p>
+                            </div>
+                            <div className="email-wrapper">
+                              <h4>Email</h4>
+                              <p>selind.black@gmail.com</p>
+                            </div>
+                    </div>
+                    <div className="profile-information-block__content-right">
+                        <a href="#blank">Редагувати профіль &rang;</a>
+                    </div>
+                </div>
+                <h2>Мої гуртки</h2>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
