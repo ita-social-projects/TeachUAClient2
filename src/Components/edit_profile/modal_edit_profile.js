51d9557949c366e6e8241a07d9b25947db4daccc
grishanych <alsolo2011@gmail.com>
2021-11-21 22:49:52 +0200

    Add styles to form

--- src/Components/edit_profile/modal_edit_profile.js
+++ src/Components/edit_profile/modal_edit_profile.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from "react";
 import { Component } from "react/cjs/react.production.min";
-import { Modal } from 'antd';
+import { Modal } from "antd";
 import FormEditProfile from "./form_edit_profile";
-import 'antd/dist/antd.css';
-import './edit_profile.scss'
+import "antd/dist/antd.css";
+import "./edit_profile.scss";
 
 class ModalEditProfile extends Component {
-    state = {
-        showEditProfileModal: false
-    }
-    handleShowEditProfileModal = () => {
-        this.setState({ showEditProfileModal: !this.state.showEditProfileModal });
-    };
-    render() {
-        return (
-            <div>
-                <a onClick={this.handleShowEditProfileModal} >Редагувати профіль</a>
-                <Modal
-                    width={880}
-                    className='edit_profile__modal'
-                    visible={this.state.showEditProfileModal}
-                    onCancel={this.handleShowEditProfileModal}
-                    footer={null} >
-                    <FormEditProfile />
-                </Modal>
-            </div>
-        )
-    }
+  state = {
+    showEditProfileModal: false,
+  };
+  handleShowEditProfileModal = () => {
+    this.setState({ showEditProfileModal: !this.state.showEditProfileModal });
+  };
+  render() {
+    return (
+      <div>
+        <a onClick={this.handleShowEditProfileModal}>Редагувати профіль</a>
+        <Modal
+          width={880}
+          className="edit_profile__modal"
+          visible={this.state.showEditProfileModal}
+          onCancel={this.handleShowEditProfileModal}
+          footer={null}
+        >
+          <FormEditProfile />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default ModalEditProfile;
--- src/Components/edit_profile/profile.js
+++ src/Components/edit_profile/profile.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Component } from 'react';
+import { Component } from "react";
 import ModalEditProfile from "./modal_edit_profile";
 
 class Profile extends Component {
-    render() {
-        return (
-            <ModalEditProfile />
-        )
-    }
+  render() {
+    return <ModalEditProfile />;
+  }
 }
 
 export default Profile;
