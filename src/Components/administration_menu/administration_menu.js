d8d139a922a9237b7dd123a9c577016c61152319
grishanych <alsolo2011@gmail.com>
2021-11-29 22:25:16 +0200

    Changed files names

--- src/Components/administration_menu/administration_menu.js
+++ src/Components/administration_menu/administration_menu.js
@@ -2,27 +2,28 @@ import React from "react";
 import { Component } from "react";
 import { Menu } from "antd";
 import "antd/dist/antd.css";
-import "./administration_menu.scss";
+import "./administration-menu.scss";
 
 const { SubMenu } = Menu;
 
-
-class Administration_menu extends Component {
+class AdministrationMenu extends Component {
   render() {
     return (
-      <Menu mode="vertical" className='menu_administration'>
-        <SubMenu key="sub_administration" title="Адміністрування" className='sub_menu_administration'>
-          <Menu.Item className='item_menu_administration' key="1">Міста</Menu.Item>
-          <Menu.Item key="2">Райони</Menu.Item>
-          <Menu.Item key="3">Станції/Місцевості</Menu.Item>
-          <Menu.Item key="4">Категорії</Menu.Item>
-          <Menu.Item key="5">Контакти</Menu.Item>
-          <Menu.Item key="6">Користувачі</Menu.Item>
-          <Menu.Item key="7">FAQ</Menu.Item>
-          <Menu.Item key="8">Імпортувати дані</Menu.Item>
-          <Menu.Item key="9">Експортувати дані</Menu.Item>
-          <Menu.Item key="10">Підтвердження</Menu.Item>
-          <Menu.Item key="11">Зміна власника</Menu.Item>
+      <Menu mode="vertical" className='menu-administration'>
+        <SubMenu key="sub_administration" title="Адміністрування" className='sub-menu-administration'>
+          <Menu.Item className='sub-menu-administration' key="2">Райони</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="3">Станції/Місцевості</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="4">Категорії</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="5">Контакти</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="6">Користувачі</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="7">FAQ</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="8">Імпортувати дані</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="9">Експортувати дані</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="10">Підтвердження</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="11">Зміна власника</Menu.Item>
+          <Menu.Item className='sub-menu-administration' key="1">Міста</Menu.Item>
+
+          
         </SubMenu>
       </Menu>
 
@@ -32,4 +33,4 @@ class Administration_menu extends Component {
   }
 }
 
-export default Administration_menu;
+export default AdministrationMenu;
