9045204af00963ba294f0e0024da0b1a7f44b471
grishanych <alsolo2011@gmail.com>
2021-12-03 17:16:42 +0200

    Removed file-saver, made part of url as a variable

--- src/Components/administrationMenu/administrationExport/administrationExport.js
+++ src/Components/administrationMenu/administrationExport/administrationExport.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Component } from "react";
 
+const linkHost = 'http://localhost:3000';
+
 class AdministrationExportData extends Component {
   render() {
     return (
       <div>
-        <a
-          href="http://localhost:3000/dev/admin/export-database.html"
-          download
-        ></a>
+        <a        href={linkHost, '/dev/admin/export-database.html'} download>
+        </a>
       </div>
     );
   }
