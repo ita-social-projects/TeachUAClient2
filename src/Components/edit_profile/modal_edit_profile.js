e6b070760edf172db9dba62678478f7a4fd11557
grishanych <alsolo2011@gmail.com>
2021-12-04 06:49:05 +0200

    Fixed styles

--- src/Components/edit_profile/modal_edit_profile.js
+++ src/Components/edit_profile/modal_edit_profile.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Component } from "react";
-import { Modal } from "antd";
+import { Button, Modal } from "antd";
 import FormEditProfile from "./form_edit_profile";
 import "antd/dist/antd.css";
 import "./edit_profile.scss";
-import {
-    ArrowRightOutlined
-  } from "@ant-design/icons";
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 class ModalEditProfile extends Component {
   state = {
@@ -17,11 +15,16 @@ class ModalEditProfile extends Component {
   };
   render() {
     return (
-      <div>
-        <a onClick={this.handleShowEditProfileModal}>Редагувати профіль &nbsp; <ArrowRightOutlined /></a>
+      <div className="profile-information-block__content-right">
+        <Button
+          className="edit-profile-button"
+          onClick={this.handleShowEditProfileModal}
+        >
+          Редагувати профіль &nbsp; <ArrowRightOutlined />
+        </Button>
         <Modal
           width={880}
-          className="edit_profile__modal"
+          className="edit-profile-modal"
           visible={this.state.showEditProfileModal}
           onCancel={this.handleShowEditProfileModal}
           footer={null}
