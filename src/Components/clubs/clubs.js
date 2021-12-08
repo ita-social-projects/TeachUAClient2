b0f58b7918481cde770f5cb0c2313ab782310df4
grishanych <alsolo2011@gmail.com>
2021-12-08 07:24:59 +0200

    Add LeftSearch to Clubs

--- src/Components/clubs/clubs.js
+++ src/Components/clubs/clubs.js
@@ -2,10 +2,16 @@ import React from 'react'
 import 'antd/dist/antd.css';
 import "./clubs.scss";
 import ClubsItem from "../clubs/clubsItem/clubsItem";
+import { LeftSearch } from '../left_side_search/left_side_search';
+
+
 class Clubs extends React.Component {
   render() {
     return (
       <div className="clubs-wrapper">
+        <div>
+          <LeftSearch />
+        </div>
         <div className="clubs">
           <ClubsItem></ClubsItem>
        </div>
