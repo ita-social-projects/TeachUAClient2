067a897d465580b5b1c7ed8b813c239ab71ce5b4
Oleksandr Sovynskyi <oleksandrsov@gmail.com>
2021-12-16 12:46:39 +0200

    merged with develop

--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -14,7 +14,7 @@ class Login extends React.Component {
   };
 
   handleEmail = (event) => {
-    this.setState({email:event.target.value})
+    this.setState({email: event.target.value})
   }
 
   handlePassword = (event) => {
