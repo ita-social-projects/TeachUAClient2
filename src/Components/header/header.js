95e5ba567f2a95f50628aef55c7e55b0442ce71c
Oleksandr Sovynskyi <oleksandrsov@gmail.com>
2021-12-22 20:40:54 +0200

    created services for putdata

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Menu, Dropdown } from "antd";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import Login from "../login/login";
 import Registration from "../registration/Registration";
@@ -19,141 +19,140 @@ import Lens from "../header_img/lens.svg";
 import Toggle from "../header_img/toggle.svg";
 import Plate from "../header_img/plate.svg";
 import menuIcon from "../header_img/menu.svg";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./header.scss";
 
-
-
 export class header extends Component {
-    logout = () => {
-        localStorage.removeItem("accessToken");
-        window.location.reload();
-    }
+  logout = () => {
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+  };
 
-    isAuthorather = () => {
-        return localStorage.getItem("accessToken") !== null;
-    } 
+  isAuthorather = () => {
+    return localStorage.getItem("accessToken") !== null;
+  };
 
-    render() {
-        const menu = (
-            <Menu>
-                <Menu.Item>
-                  <Link to='/challengeUA'>
-                    <a target="#blank">Навчай українською Челендж</a>
-                   </Link>
-                </Menu.Item>
-                <Menu.Item>
-                <Link to='/marathon'>
-                    <a target="#blank">Мовомаратон</a>
-                </Link>   
-                </Menu.Item>
-                <Menu.Item>
-                    <a target="#blank">Навчай українською</a>
-                </Menu.Item>
-            </Menu>
-        );
-        
-        const log = this.isAuthorather() ? (
-          <Menu>
-            
-            <Menu.Item onClick={this.logout}>
-                Вийти
-            </Menu.Item>
-            <Menu.Item>
-              <AddCenter/>
-            </Menu.Item>
-            <Menu.Item>
-                    <a href="/profile">Профіль </a>
-            </Menu.Item>
-            <Menu.Item>
-            <AddClub/>
-            </Menu.Item>
-            <Menu.Item>
-            <AdministrationMenu />
-            </Menu.Item>
-            
-            
-        
-          </Menu>
-        ):(<Menu>
-            <Menu.Item>
-              <Registration/>
-            </Menu.Item>
-            <Menu.Item>
-              <Login/>
-            </Menu.Item>
-        </Menu>);
+  render() {
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <Link to="/challengeUA">
+            <a target="#blank">Навчай українською Челендж</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/marathon">
+            <a target="#blank">Мовомаратон</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="#blank">Навчай українською</a>
+        </Menu.Item>
+      </Menu>
+    );
 
-        console.log(this.props);
-        return (
-            <div className="Header">
-                <div className="wrapper">
-                    <img className="logo" src={Logo} />
-                    <div className="Menu">
-                        <label className="burger-icon" htmlFor="burger-checkbox">
-                            <img src={menuIcon} />
-                        </label>
-                        <input type="checkbox" id="burger-checkbox" />
-                        <nav>
-                            <a href="/clubs">
-                                <img src={ProjectIcon} />
-                                Гуртки
-                            </a>
-                            <Dropdown overlay={menu}>
-                                <a className="challenge" href="#blank">
-                                    <img src={Crown} />
-                                    Челендж
-                                </a>
-                            </Dropdown>
-                            <a className="news" href="#blank">
-                                <img src={NewsIcon} />
-                                Про нас
-                            </a>
-                            <a className="project" href="#blank">
-                                <img src={Flag} />
-                                Послуги українською
-                            </a>
-                        </nav>
-                    </div>
-                    <div className="log">
-                        <div className="locationIcon">
-                            <img src={Loaction} />
-                            <div className="city">
-                                <p>Місто</p>
-                            </div>
-                        </div>
+    const log = this.isAuthorather() ? (
+      <Menu>
+        <Menu.Item>
+          <AddClub />
+        </Menu.Item>
+        <Menu.Item>
+          <AddCenter />
+        </Menu.Item>
+        <Menu.Item>
+          <a href="/profile">Профіль </a>
+        </Menu.Item>
+        <Menu.Item onClick={this.logout}>Вийти</Menu.Item>
 
-                        <Dropdown overlay={log}>
-                            <div className="avatar">
-                                <img src={Avatar} />
-                            </div>
-                        </Dropdown>
-                    </div>
-                </div>
-                <div className="LowerWrappper">
-                    <p className="caption">Ініціатива &quot;Навчай українською&quot;</p>
-                    <div className="search">
-                        <div className="input-field">
-                            <div className="search-bar">
-                                <input type="text" placeholder="Який гурток шукаєте?"></input>
-                                <img src={Lens} />
-                            </div>
-                            <img className="plaate" src={Plate} />
-                        </div>
-                        <div className="litle-toggle">
-                            <a href="#blank">
-                                <img className="toggle" src={Toggle} onClick={this.props.toggleSideSearch} />
-                            </a>
-                        </div>
-                    </div>
-                </div>
+        <Menu.Item>
+          <AdministrationMenu />
+        </Menu.Item>
+      </Menu>
+    ) : (
+      <Menu>
+        <Menu.Item>
+          <Registration />
+        </Menu.Item>
+        <Menu.Item>
+          <Login />
+        </Menu.Item>
+      </Menu>
+    );
+
+    console.log(this.props);
+    return (
+      <div className="Header">
+        <div className="wrapper">
+          <img className="logo" src={Logo} />
+          <div className="Menu">
+            <label className="burger-icon" htmlFor="burger-checkbox">
+              <img src={menuIcon} />
+            </label>
+            <input type="checkbox" id="burger-checkbox" />
+            <nav>
+              <a href="/clubs">
+                <img src={ProjectIcon} />
+                Гуртки
+              </a>
+              <Dropdown overlay={menu}>
+                <a className="challenge" href="#blank">
+                  <img src={Crown} />
+                  Челендж
+                </a>
+              </Dropdown>
+              <a className="news" href="#blank">
+                <img src={NewsIcon} />
+                Про нас
+              </a>
+              <a className="project" href="#blank">
+                <img src={Flag} />
+                Послуги українською
+              </a>
+            </nav>
+          </div>
+          <div className="log">
+            <div className="locationIcon">
+              <img src={Loaction} />
+              <div className="city">
+                <p>Місто</p>
+              </div>
             </div>
-        );
-    }
+
+            <Dropdown overlay={log}>
+              <div className="avatar">
+                <img src={Avatar} />
+              </div>
+            </Dropdown>
+          </div>
+        </div>
+        <div className="LowerWrappper">
+          <p className="caption">Ініціатива &quot;Навчай українською&quot;</p>
+          <div className="search">
+            <div className="input-field">
+              <div className="search-bar">
+                <input type="text" placeholder="Який гурток шукаєте?"></input>
+                <img src={Lens} />
+              </div>
+              <img className="plaate" src={Plate} />
+            </div>
+            <div className="litle-toggle">
+              <a href="#blank">
+                <img
+                  className="toggle"
+                  src={Toggle}
+                  onClick={this.props.toggleSideSearch}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 header.propTypes = {
-    toggleSideSearch: PropTypes.func
+  toggleSideSearch: PropTypes.func,
 };
 
 export default header;
