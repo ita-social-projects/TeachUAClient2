2af58028bb8512d40a4014c01262c23fe15ff56c
grishanych <alsolo2011@gmail.com>
2021-12-23 21:25:13 +0200

    Create edit(put)/delete

--- src/Services/questions.js
+++ src/Services/questions.js
@@ -1,17 +1,57 @@
-import fetchRequest, {REACT_APP_API_URL} from "./serviceUtills"
+import fetchRequest, { REACT_APP_API_URL } from "./serviceUtills";
 
-
-const url = `${REACT_APP_API_URL}/questions` 
+const urlGet = `${REACT_APP_API_URL}/questions`;
+const url = `${REACT_APP_API_URL}/question`;
 
 const getQuestions = () => {
-    return fetchRequest.get(url);
+  return fetchRequest.get(urlGet);
 };
 
-const createQuestions = (title, text) => {
-    return fetchRequest.post(url, {title, text});
+const updateQuestions = (data) => {
+  return fetchRequest
+    .put(`${url}/${data.id}`, {
+      id: data.id,
+      title: data.title,
+      text: data.text,
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      return error.response.data;
+    });
 };
 
-export {
-    getQuestions,
-    createQuestions,
+const deleteQuestions = (data) => {
+  return fetchRequest
+    .delete(`${url}/${data.id}`, {
+      id: data.id,
+      title: data.title,
+      text: data.text,
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      return error.response.data;
+    });
 };
+
+const createQuestions = (data) => {
+  return fetchRequest
+    .post(url, {
+      title: data.title,
+      text: data.text,
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error.response);
+      return error.response.data;
+    });
+};
+
+export { getQuestions, createQuestions, updateQuestions, deleteQuestions };
