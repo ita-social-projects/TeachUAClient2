c31cdc60d84abc7558163008aa0923fdbc5721ae
Arsen Sharifov <selind.black@gmail.com>
2022-01-25 12:56:37 +0200

    clubs

--- src/Components/clubs/clubsItem/clubsItem.js
+++ src/Components/clubs/clubsItem/clubsItem.js
@@ -8,33 +8,56 @@ import {
 } from "../../../Services/clubList";
 
 class ClubsItem extends React.Component {
+  state={ 
+    data:[],
+    currentPage:1
+  } 
+  getData(){
+    getSitiesServise(this.state.currentPage - 1).then((response) => { 
+      response.data.content.map(element => {
+        element.categories[0].urlLogo = element.categories[0].urlLogo.replace("/static","/src")}
+      );
+      this.setState({data:response.data.content})  
+      console.log(this.state.data)
+      console.log(this.state.currentPage)
+  }).catch(function(e) {
+    console.log(e);
+  })
+  }
+  componentDidMount(){ 
+    this.getData()
+  }
+    onChange = page => {
+      this.setState({currentPage:page})  
+      this.getData()
+    }
+
+  
+
   render() {
-    const data = [];
-    const getData = () => {
-      getSitiesServise().then((response) => {
-        response.data.content.map((post) => {
-          data.push(post);
-      });
-      console.log(data)
-    })};
-    getData()
-   
-    
+    const getShortContent = (content) => {
+      const contentObject = JSON.parse(content);
+      return contentObject.blocks[3].text
+    }
     return (
       <List
         className="card"
-        pagination={{ pageSize: 8 }}
-        dataSource={data}
+        pagination={{ 
+          pageSize: 8,
+          total:75,
+          onChange: this.onChange
+         }}
+        
+        dataSource={this.state.data}
         itemLayout="vertical"
         size="large"
+        
         renderItem={(item) => (
-          <Card key="{item.id}">
+          <Card key={item.number}>
             <div className="title">
-              <div
-                className="title__img"
-                // style={{ backgroundColor: item.bgIconTitle }}
-              >
-                {/* <img src={item.pathIconTitle} /> */}
+              <div className="title__img"
+                 style={{ backgroundColor: item.categories[0].backgroundColor }} >
+                 <img src={item.categories[0].urlLogo} /> 
               </div>
               <p>{item.name}</p>
             </div>
@@ -44,11 +67,11 @@ class ClubsItem extends React.Component {
                 <p>Спортивні секції</p>
               </div>
             </div>
-            <p className="description">{item.description}</p>
+            <p className="description">{getShortContent(item.description)}</p>
             <Rate />
             <div className="adress">
               <img src="/src/images/map/cluster.png" />
-              <p>{item.locations}</p>
+               <p>{item.locations[0].cityName}, {item.locations[0].address}</p> 
             </div>
 
             <button className="more-info">
--- src/Services/clubList.js
+++ src/Services/clubList.js
@@ -1,10 +1,10 @@
 import fetchRequest from "./serviceUtills";
 import { REACT_APP_API_URL } from "./serviceUtills";
 
-const urlSearch = `${REACT_APP_API_URL}/clubs/search?clubName=&cityName=%D0%9A%D0%B8%D1%97%D0%B2&isOnline=false&categoryName=&page=0`;
+const urlSearch = `${REACT_APP_API_URL}/clubs/search?clubName=&cityName=%D0%9A%D0%B8%D1%97%D0%B2&isOnline=false&categoryName=&page=`;
 
-const getSitiesServise = () => {
-  return fetchRequest.get(urlSearch);
+const getSitiesServise = (number) => {
+  return fetchRequest.get(urlSearch + number);
 };
 
 export { getSitiesServise};
